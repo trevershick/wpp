d842b57e79b133fae0c529e8d529d2daa04a52c5
Trever Shick <trever.shick@tanium.com>
2021-09-17 03:54:42 -0500

    fix: add store_test_results
    Fixes #2

--- test/check_main.c
+++ test/check_main.c
@@ -18,8 +18,13 @@ int main(void) {
 
   ADD_SUITE(sr, make_format_test_suite);
 
-  srunner_set_tap(sr, "tests.tap");
-  srunner_set_xml(sr, "tests.xml");
+  const char* xml_output = getenv("XML_OUTPUT_FILE");
+  if (!xml_output) {
+    xml_output = "tests.xml";
+  }
+  fprintf(stdout, "Writing test output to : %s\n", xml_output);
+  //srunner_set_tap(sr, "tests.tap");
+  srunner_set_xml(sr, xml_output);
   srunner_run_all(sr, CK_VERBOSE /*CK_NORMAL*/);
   int number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
