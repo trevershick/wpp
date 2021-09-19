3e1d3591106dd1295570f866a368cdea3cf4276a
Trever Shick <trever.shick@tanium.com>
2021-09-19 12:50:21 -0500

    add more tests

--- test/check_main.c
+++ test/check_main.c
@@ -1,37 +1,39 @@
 #include <check.h>
-#include <stdlib.h>
 #include <limits.h>
-#include <stdio.h>
 #include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define ADD_SUITE(runner, factory_method)                                                                              \
-  {                                                                                                                    \
-    struct Suite *suite = factory_method();                                                                            \
-    srunner_add_suite(runner, suite);                                                                                  \
+#define ADD_SUITE(runner, factory_method)                                      \
+  {                                                                            \
+    struct Suite *suite = factory_method();                                    \
+    srunner_add_suite(runner, suite);                                          \
   }
 
-
 struct Suite *make_format_test_suite();
-
+struct Suite *make_context_test_suite();
 
 int main(void) {
   struct Suite *s = make_format_test_suite();
   SRunner *sr = srunner_create(s);
 
+  s = make_context_test_suite();
+  ADD_SUITE(sr, make_context_test_suite);
+
   char xml_output[PATH_MAX];
   memset(xml_output, 0, PATH_MAX);
-  const char* outputs_dir = getenv("TEST_UNDECLARED_OUTPUTS_DIR");
+  const char *outputs_dir = getenv("TEST_UNDECLARED_OUTPUTS_DIR");
   if (outputs_dir) {
-    sprintf(xml_output, "%s/results.xml", outputs_dir);  
+    sprintf(xml_output, "%s/results.xml", outputs_dir);
   } else {
-    sprintf(xml_output, "results.xml");  
+    sprintf(xml_output, "results.xml");
   }
 
   char output_path[PATH_MAX];
   memset(output_path, 0, PATH_MAX);
   realpath(output_path, xml_output);
   fprintf(stdout, "Writing test output to : %s\n", xml_output);
-  //srunner_set_tap(sr, "tests.tap");
+  // srunner_set_tap(sr, "tests.tap");
   srunner_set_xml(sr, xml_output);
   srunner_set_xml_format(sr, CK_XML_FORMAT_JUNIT);
   srunner_run_all(sr, CK_VERBOSE /*CK_NORMAL*/);
@@ -39,4 +41,3 @@ int main(void) {
   srunner_free(sr);
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
---
+++ test/context_test.c
@@ -0,0 +1,65 @@
+#include "src/context.h"
+
+#include <check.h>
+#include <string.h>
+
+#include "check_macros.h"
+
+START_TEST(test_init_context_usessetrcfile) {
+  struct Context c = {0};
+  strcpy(c.rc_file, "~/trevers_rc");
+  int result = init_context(&c);
+  ck_assert_msg(strstr(c.rc_file, "trevers_rc") != NULL,
+                "RC file should not override explicit setting but is '%s'",
+                c.rc_file);
+  ck_assert_msg(strstr(c.rc_file, "~") == NULL,
+                "RC file should have been 'expanded'");
+  // should fail loading the file
+  ck_assert_int_eq(1, result);
+}
+END_TEST
+
+START_TEST(test_init_context_default) {
+  struct Context c = {0};
+  init_context(&c);
+  ck_assert_msg(strstr(c.rc_file, "workspaces.rc") != NULL,
+                "RC file should contain workspaces.rc but is '%s'", c.rc_file);
+  ck_assert_msg(strstr(c.rc_file, "~") == NULL,
+                "RC file should have been 'expanded'");
+  ck_assert_msg(c.cwd[0] != 0, "CWD should be populated");
+}
+END_TEST
+
+START_TEST(test_init_context_opensfile) {
+  char buffer[80] = {0};
+  // write a file
+  {
+    struct Context c = {0};
+    strcpy(c.rc_file, "$TEST_TMPDIR/file_to_open");
+    ck_assert_int_eq(1, init_context(&c));
+    ck_assert(strstr(c.rc_file, "TEST") == NULL);
+    FILE *out = fopen(c.rc_file, "w");
+    ck_assert(out != NULL);
+    fprintf(out, "nothing special");
+    fclose(out);
+  }
+
+  {
+    struct Context c = {0};
+    strcpy(c.rc_file, "$TEST_TMPDIR/file_to_open");
+    ck_assert_int_eq(0, init_context(&c));
+    fgets(buffer, sizeof(buffer), c.file);
+    destroy_context(&c);
+    ck_assert_str_eq(buffer, "nothing special");
+  }
+}
+END_TEST
+// clang-format off
+CK_SUITE(context_test,
+  CK_CASE(init_context,
+    CK_TEST(test_init_context_default)
+    CK_TEST(test_init_context_usessetrcfile)
+    CK_TEST(test_init_context_opensfile)
+  )
+)
+// clang-format on
