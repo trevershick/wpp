9c7873f8a44ce0e7a93fe1a827ff5cddf9020e03
Trever Shick <trever.shick@tanium.com>
2022-06-15 17:52:49 -0500

    more

--- src/lib/format.c
+++ src/lib/format.c
@@ -4,7 +4,7 @@
 
 int is_section_header(const char *line) { return *line == '['; }
 
-int is_matching_section_header(const char *line, const char *section) {
+int is_matching_sechdr(const char *line, const char *section) {
   int sz;
   return *line == '[' && (sz = strlen(section)) > 0 &&
          strncmp(line + 1, section, sz) == 0 && line[sz + 1] == ']';
